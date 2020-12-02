28e1387b5cfc99672debdf9add8eea8b8d679202
swardbladpaulo <paulo@swardblads.se>
2020-12-02 20:18:05 +0100

    trobleshooting connection to heroku

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+
+let apiUrl;
+//if (process.env.NODE_ENV === "production") {
+  apiUrl = "https://trumpster-dump-api.herokuapp.com/api/";
+//} else {
+//  apiUrl = "http://localhost:3000/api/";
+//}
+axios.defaults.baseURL = apiUrl;
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,3 +20,6 @@ ReactDOM.render(
 );
 
 reportWebVitals();
+
+
+//I need to comment out line 10,12,13,14 to be able to login, otherwise it default to the else statement
