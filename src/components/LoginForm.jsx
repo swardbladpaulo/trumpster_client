5713d69bf53d7a63d71acb5bdfee68b8b6059c1f
Sanne <sannelindholm@hotmail.com>
2020-12-04 20:38:54 +0100

    Changing component to a class. Adding function to render loginform when login button clicked, creating a post request for user credentials and adding an error message when entered wrong password.

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 
+class LoginForm extends Component {
+  state = {
+    showLoginButton: false,
+    message: "",
+  };
 
-const LoginForm = () => {
-  return (
-    <div>
-      <form
+  async authenticate(event) {
+    event.preventDefault();
+    let credentials = {
+      email: event.target.email.value,
+      password: event.target.password.value,
+    };
+    try {
+      let response = await axios.post("/auth", credentials);
+      let userData = {
+        uid: response.headers.uid,
+        client: response.headers.client,
+        token_type: response.headers.token_type,
+        expiry: response.headers.expiry,
+      };
+      localStorage.setItem("credentials", JSON.stringify(userData));
+      localStorage.setItem("authenticated", true);
+      this.setState({
+        showLoginButton: false,
+        message: "TREMENDOUS, you are now a registered Trumpster!",
+      });
+    } catch (error) {
+      localStorage.setItem("authenticated", false);
+      this.setState({ message: "Your login credentials are RIGGED!!" });
+    }
+  }
+  render() {
+    return (
+      <>
+        {this.state.showLoginButton ? (
+          <form
             data-cy="login-form"
+            onSubmit={(event) => this.authenticate(event)}
           >
             <input
               type="email"
@@ -23,8 +56,19 @@ const LoginForm = () => {
               Submit
             </button>
           </form>
-    </div>
-  )
+        ) : (
+          <button
+            data-cy="login-btn"
+            onClick={() => this.setState({ showLoginButton: true })}
+          >
+            Login
+          </button>
+        )}
+        {this.state.message && (
+          <div data-cy="error-message">{this.state.message}</div>
+        )}
+      </>
+    )
+  }
 }
-
 export default LoginForm
