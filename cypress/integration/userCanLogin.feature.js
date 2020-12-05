5698b7509d6282fd9f740ff6c05bb04d0e9724d1
viktor <viktorkarlsson125@gmail.com>
2020-12-05 15:57:21 +0100

    formating code

--- cypress/integration/userCanLogin.feature.js
+++ cypress/integration/userCanLogin.feature.js
@@ -1,57 +1,57 @@
 describe("User can login", () => {
-	beforeEach(() => {
-		cy.server();
-		cy.route({
-			method: "GET",
-			url: "http://localhost:3000/api/quotes/random",
-			response: "fixture:display_one_quote.json",
-		});
-		cy.route({
-			method: "POST",
-			url: "http://localhost:3000/api/auth",
-			response: "fixture:user_can_login.json",
-			headers: {
-				uid: "user@gmail.com",
-				access_token: "blabla",
-				client: "1337",
-				token_type: "Bearer",
-				expiry: 169999,
-			},
-		});
-		cy.visit("/");
-	});
+  beforeEach(() => {
+    cy.server();
+    cy.route({
+      method: "GET",
+      url: "http://localhost:3000/api/quotes/random",
+      response: "fixture:display_one_quote.json",
+    });
+    cy.route({
+      method: "POST",
+      url: "http://localhost:3000/api/auth",
+      response: "fixture:user_can_login.json",
+      headers: {
+        uid: "user@gmail.com",
+        access_token: "blabla",
+        client: "1337",
+        token_type: "Bearer",
+        expiry: 169999,
+      },
+    });
+    cy.visit("/");
+  });
 
-	it("successfully", () => {
-		cy.get("[data-cy='login-btn']").click();
-		cy.get("[data-cy='login-form']").within(() => {
-			cy.get("[data-cy='email']").type("user@gmail.com");
-			cy.get("[data-cy='password']").type("password");
-			cy.get("[data-cy='submit-btn']").contains("Submit").click();
-		});
-		cy.get("[data-cy='error-message']").contains(
-			"You have successfully logged in!"
-		);
-	});
+  it("successfully", () => {
+    cy.get("[data-cy='login-btn']").click();
+    cy.get("[data-cy='login-form']").within(() => {
+      cy.get("[data-cy='email']").type("user@gmail.com");
+      cy.get("[data-cy='password']").type("password");
+      cy.get("[data-cy='submit-btn']").contains("Submit").click();
+    });
+    cy.get("[data-cy='error-message']").contains(
+      "You have successfully logged in!"
+    );
+  });
 
-	it("sad path: unsuccessfully", () => {
-		cy.route({
-			method: "POST",
-			url: "http://localhost:3000/api/auth",
-			status: "401",
-			response: {
-				errors: ["Your login credentials are RIGGED"],
-				success: false,
-			},
-		});
+  it("sad path: unsuccessfully", () => {
+    cy.route({
+      method: "POST",
+      url: "http://localhost:3000/api/auth",
+      status: "401",
+      response: {
+        errors: ["Your login credentials are RIGGED"],
+        success: false,
+      },
+    });
 
-		cy.get("[data-cy='login-btn']").click();
-		cy.get("[data-cy='login-form']").within(() => {
-			cy.get("[data-cy='email']").type("user@gmail.com");
-			cy.get("[data-cy='password']").type("wrongpassword");
-			cy.get("button").contains("Submit").click();
-		});
-		cy.get("[data-cy='error-message']").contains(
-			"Your login credentials are RIGGED!!"
-		);
-	});
+    cy.get("[data-cy='login-btn']").click();
+    cy.get("[data-cy='login-form']").within(() => {
+      cy.get("[data-cy='email']").type("user@gmail.com");
+      cy.get("[data-cy='password']").type("wrongpassword");
+      cy.get("button").contains("Submit").click();
+    });
+    cy.get("[data-cy='error-message']").contains(
+      "Your login credentials are RIGGED!!"
+    );
+  });
 });
--- src/components/QuotesData.jsx
+++ src/components/QuotesData.jsx
@@ -2,21 +2,21 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class QuotesData extends Component {
-	state = {
-		quote: "",
-	};
-	async getQuotes() {
-		let response = await axios.get("/quotes/random");
-		this.setState({ quote: response.data });
-	}
+  state = {
+    quote: "",
+  };
+  async getQuotes() {
+    let response = await axios.get("/quotes/random");
+    this.setState({ quote: response.data });
+  }
 
-	componentDidMount() {
-		this.getQuotes();
-	}
+  componentDidMount() {
+    this.getQuotes();
+  }
 
-	render() {
-		return <div data-cy="random_quote">{this.state.quote.random_quote}</div>;
-	}
+  render() {
+    return <div data-cy="random_quote">{this.state.quote.random_quote}</div>;
+  }
 }
 
 export default QuotesData;
