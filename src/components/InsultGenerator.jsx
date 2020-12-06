f508be1a3fb916147c1c9d19764044c63f968b92
Sanne <sannelindholm@hotmail.com>
2020-12-06 19:20:42 +0100

    Adding more quotes to the insult generator

--- src/components/InsultGenerator.jsx
+++ src/components/InsultGenerator.jsx
@@ -17,6 +17,9 @@ class InsultGenerator extends Component {
       `${this.state.name}, didn’t have the mental capacity needed. You were dumb as a rock and I couldn’t get rid of you fast enough. You were lazy as hell`,
       `${this.state.name} went in and didn’t know the air conditioning didn’t work. You sweated like a dog… How are you gonna beat ISIS? I don’t think it’s gonna happen.`,
       `If I were running ‘The View’, I’d fire ${this.state.name}. I mean, I’d look at you right in that fat, ugly face of yours, I’d say ${this.state.name}, you’re fired.`,
+      `An ''extremely credible source'' has called my office and told me that ${this.state.name}'s birth certificate is a fraud.`,
+      `${this.state.name} is an extremely unattractive woman, I refuse to say that because I always insist on being politically correct.`,
+      `I am watching Crooked ${this.state.name} speak. Same old stuff, our country needs change!`
     ];
     const result = insults[Math.floor(Math.random() * 3)]
     this.setState({ insultResult: result });
